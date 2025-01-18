f3f9888ea8845ea97369868d8bd0070bdaf1cde8
Philip Woulfe <philip.woulfe@gmail.com>
2025-01-18 18:40:30 +0000

    AB#15 add missing driver ids...again

--- src/ErgastApi/Ids/Drivers.cs
+++ src/ErgastApi/Ids/Drivers.cs
@@ -6,14 +6,59 @@
     public static class Drivers
     {
         /// <summary>
+        /// Driver ID for Alain Prost.
+        /// </summary>
+        public const string AlainProst = "prost";
+
+        /// <summary>
+        /// Driver ID for Alberto Ascari.
+        /// </summary>
+        public const string AlbertoAscari = "fangio";
+
+        /// <summary>
+        /// Driver ID for Alexander Albon.
+        /// </summary>
+        public const string AlexanderAlbon = "albon";
+
+        /// <summary>
+        /// Driver ID for Andrea Kimi Antonelli.
+        /// </summary>
+        public const string AndreaKimiAntonelli = "antonelli";
+
+        /// <summary>
+        /// Driver ID for Anthony Davidson.
+        /// </summary>
+        public const string AnthonyDavidson = "davidson";
+
+        /// <summary>
         /// Driver ID for Antonio Giovinazzi.
         /// </summary>
         public const string AntonioGiovinazzi = "giovinazzi";
 
         /// <summary>
+        /// Driver ID for Ayrton Senna.
+        /// </summary>
+        public const string AyrtonSenna = "senna";
+
+        /// <summary>
+        /// Driver ID for Bruno Senna.
+        /// </summary>
+        public const string BrunoSenna = "bruno_senna";
+
+        /// <summary>
         /// Driver ID for Carlos Sainz.
         /// </summary>
         public const string CarlosSainz = "sainz";
+
+        /// <summary>
+        /// Driver ID for Charles Leclerc.
+        /// </summary>
+        public const string CharlesLeclerc = "leclerc";
+
+        /// <summary>
+        /// Driver ID for Damon Hill.
+        /// </summary>
+        public const string DamonHill = "damon_hill";
 
         /// <summary>
         /// Driver ID for Daniel Ricciardo.
@@ -24,6 +69,21 @@
         /// Driver ID for Daniil Kvyat.
         /// </summary>
         public const string DaniilKvyat = "kvyat";
+
+        /// <summary>
+        /// Driver ID for David Coulthard.
+        /// </summary>
+        public const string DavidCoulthard = "coulthard";
+
+        /// <summary>
+        /// Driver ID for Eddie Irvine.
+        /// </summary>
+        public const string EddieIrvine = "irvine";
+
+        /// <summary>
+        /// Driver ID for Emerson Fittipaldi.
+        /// </summary>
+        public const string EmersonFittipaldi = "emerson_fittipaldi";
 
         /// <summary>
         /// Driver ID for Esteban Gutierrez.
@@ -51,6 +111,51 @@
         public const string FernandoAlonso = "alonso";
 
         /// <summary>
+        /// Driver ID for Giedo van der Garde.
+        /// </summary>
+        public const string GiedoVanDerGarde = "garde";
+
+        /// <summary>
+        /// Driver ID for Giancarlo Fisichella.
+        /// </summary>
+        public const string GiancarloFisichella = "fisichella";
+
+        /// <summary>
+        /// Driver ID for Graham Hill.
+        /// </summary>
+        public const string GrahamHill = "hill";
+
+        /// <summary>
+        /// Driver ID for Heikki Kovalainen.
+        /// </summary>
+        public const string HeikkiKovalainen = "kovalainen";
+
+        /// <summary>
+        /// Driver ID for Heinz-Harald Frentzen.
+        /// </summary>
+        public const string HeinzHaraldFrentzen = "frentzen";
+
+        /// <summary>
+        /// Driver ID for Jack Brabham.
+        /// </summary>
+        public const string JackBrabham = "jack_brabham";
+
+        /// <summary>
+        /// Driver ID for Jackie Stewart.
+        /// </summary>
+        public const string JackieStewart = "stewart";
+
+        /// <summary>
+        /// Driver ID for Jacques Villeneuve.
+        /// </summary>
+        public const string JacquesVilleneuve = "villeneuve";
+
+        /// <summary>
+        /// Driver ID for James Hunt.
+        /// </summary>
+        public const string JamesHunt = "hunt";
+
+        /// <summary>
         /// Driver ID for Jean-Eric Vergne.
         /// </summary>
         public const string JeanEricVergne = "vergne";
@@ -61,9 +166,34 @@
         public const string JensonButton = "button";
 
         /// <summary>
+        /// Driver ID for Jim Clark.
+        /// </summary>
+        public const string JimClark = "clark";
+
+        /// <summary>
+        /// Driver ID for John Surtees.
+        /// </summary>
+        public const string JohnSurtees = "surtees";
+
+        /// <summary>
         /// Driver ID for Jolyon Palmer.
         /// </summary>
         public const string JolyonPalmer = "jolyon_palmer";
+
+        /// <summary>
+        /// Driver ID for Jos Verstappen.
+        /// </summary>
+        public const string JosVerstappen = "verstappen";
+
+        /// <summary>
+        /// Driver ID for Juan Fangio.
+        /// </summary>
+        public const string JuanFangio = "fangio";
+
+        /// <summary>
+        /// Driver ID for Juan Pablo Montoya.
+        /// </summary>
+        public const string JuanPabloMontoya = "montoya";
 
         /// <summary>
         /// Driver ID for Jules Bianchi.
@@ -74,6 +204,11 @@
         /// Driver ID for Kamui Kobayashi.
         /// </summary>
         public const string KamuiKobayashi = "kobayashi";
+
+        /// <summary>
+        /// Driver ID for Keke Rosberg.
+        /// </summary>
+        public const string KekeRosberg = "keke_rosberg";
 
         /// <summary>
         /// Driver ID for Kevin Magnussen.
@@ -91,6 +226,11 @@
         public const string LanceStroll = "stroll";
 
         /// <summary>
+        /// Driver ID for Lando Norris.
+        /// </summary>
+        public const string LandoNorris = "norris";
+
+        /// <summary>
         /// Driver ID for Lewis Hamilton.
         /// </summary>
         public const string LewisHamilton = "hamilton";
@@ -99,6 +239,11 @@
         /// Driver ID for Marcus Ericsson.
         /// </summary>
         public const string MarcusEricsson = "ericsson";
+
+        /// <summary>
+        /// Driver ID for Mario Andretti.
+        /// </summary>
+        public const string MarioAndretti = "mario_andretti";
 
         /// <summary>
         /// Driver ID for Max Chilton.
@@ -111,6 +256,36 @@
         public const string MaxVerstappen = "max_verstappen";
 
         /// <summary>
+        /// Driver ID for Michael Schumacher.
+        /// </summary>
+        public const string MichaelSchumacher = "michael_schumacher";
+
+        /// <summary>
+        /// Driver ID for Mika Hakkinen.
+        /// </summary>
+        public const string MikaHakkinen = "hakkinen";
+
+        /// <summary>
+        /// Driver ID for Nelson Piquet.
+        /// </summary>
+        public const string NelsonPiquet = "piquet";
+
+        /// <summary>
+        /// Driver ID for Nelson Piquet Jr.
+        /// </summary>
+        public const string NelsonPiquetJr = "piquet_jr";
+
+        /// <summary>
+        /// Driver ID for Nick Heidfeld.
+        /// </summary>
+        public const string NickHeidfeld = "heidfeld";
+
+        /// <summary>
+        /// Driver ID for Nicolas Kiesa.
+        /// </summary>
+        public const string NicolasKiesa = "kiesa";
+
+        /// <summary>
         /// Driver ID for Nico Hulkenberg.
         /// </summary>
         public const string NicoHulkenberg = "hulkenberg";
@@ -119,6 +294,11 @@
         /// Driver ID for Nico Rosberg.
         /// </summary>
         public const string NicoRosberg = "rosberg";
+
+        /// <summary>
+        /// Driver ID for Olivier Panis.
+        /// </summary>
+        public const string OlivierPanis = "panis";
 
         /// <summary>
         /// Driver ID for Pascal Wehrlein.
@@ -136,14 +316,34 @@
         public const string PaulDiResta = "resta";
 
         /// <summary>
+        /// Driver ID for Ralf Schumacher.
+        /// </summary>
+        public const string RalfSchumacher = "ralf_schumacher";
+
+        /// <summary>
         /// Driver ID for Rio Haryanto.
         /// </summary>
         public const string RioHaryanto = "haryanto";
 
         /// <summary>
+        /// Driver ID for Robert Kubica.
+        /// </summary>
+        public const string RobertKubica = "kubica";
+
+        /// <summary>
         /// Driver ID for Romain Grosjean.
         /// </summary>
         public const string RomainGrosjean = "grosjean";
+
+        /// <summary>
+        /// Driver ID for Rubens Barrichello.
+        /// </summary>
+        public const string RubensBarrichello = "barrichello";
+
+        /// <summary>
+        /// Driver ID for Scott Speed.
+        /// </summary>
+        public const string ScottSpeed = "speed";
 
         /// <summary>
         /// Driver ID for Sebastian Vettel.
@@ -161,6 +361,11 @@
         public const string StoffelVandoorne = "vandoorne";
 
         /// <summary>
+        /// Driver ID for Timo Glock.
+        /// </summary>
+        public const string TimoGlock = "glock";
+
+        /// <summary>
         /// Driver ID for Valtteri Bottas.
         /// </summary>
         public const string ValtteriBottas = "bottas";
@@ -171,208 +376,8 @@
         public const string WillStevens = "stevens";
 
         /// <summary>
-        /// Driver ID for Heikki Kovalainen.
+        /// Driver ID for Yuki Tsunoda.
         /// </summary>
-        public const string HeikkiKovalainen = "kovalainen";
-
-        /// <summary>
-        /// Driver ID for Giedo van der Garde.
-        /// </summary>
-        public const string GiedoVanDerGarde = "garde";
-
-        /// <summary>
-        /// Driver ID for Marc Webber.
-        /// </summary>
-        public const string MarcWebber = "webber";
-
-        /// <summary>
-        /// Driver ID for Timo Glock.
-        /// </summary>
-        public const string TimoGlock = "glock";
-
-        /// <summary>
-        /// Driver ID for Michael Schumacher.
-        /// </summary>
-        public const string MichaelSchumacher = "michael_schumacher";
-
-        /// <summary>
-        /// Driver ID for Ralf Schumacher.
-        /// </summary>
-        public const string RalfSchumacher = "ralf_schumacher";
-
-        /// <summary>
-        /// Driver ID for Bruno Senna.
-        /// </summary>
-        public const string BrunoSenna = "bruno_senna";
-
-        /// <summary>
-        /// Driver ID for Nick Heidfeld.
-        /// </summary>
-        public const string NickHeidfeld = "heidfeld";
-
-        /// <summary>
-        /// Driver ID for Rubens Barrichello.
-        /// </summary>
-        public const string RubensBarrichello = "barrichello";
-
-        /// <summary>
-        /// Driver ID for Jarno Trulli.
-        /// </summary>
-        public const string JarnoTrulli = "trulli";
-
-        /// <summary>
-        /// Driver ID for Robert Kubica.
-        /// </summary>
-        public const string RobertKubica = "kubica";
-
-        /// <summary>
-        /// Driver ID for Giancarlo Fisichella.
-        /// </summary>
-        public const string GiancarloFisichella = "fisichella";
-
-        /// <summary>
-        /// Driver ID for Nelson Piquet Jr.
-        /// </summary>
-        public const string NelsonPiquetJr = "piquet_jr";
-
-        /// <summary>
-        /// Driver ID for David Coulthard.
-        /// </summary>
-        public const string DavidCoulthard = "coulthard";
-
-        /// <summary>
-        /// Driver ID for Anthony Davidson.
-        /// </summary>
-        public const string AnthonyDavidson = "davidson";
-
-        /// <summary>
-        /// Driver ID for Scott Speed.
-        /// </summary>
-        public const string ScottSpeed = "speed";
-
-        /// <summary>
-        /// Driver ID for Juan Pablo Montoya.
-        /// </summary>
-        public const string JuanPabloMontoya = "montoya";
-
-        /// <summary>
-        /// Driver ID for Jacques Villeneuve.
-        /// </summary>
-        public const string JacquesVilleneuve = "villeneuve";
-
-        /// <summary>
-        /// Driver ID for Heinz-Harald Frentzen.
-        /// </summary>
-        public const string HeinzHaraldFrentzen = "frentzen";
-
-        /// <summary>
-        /// Driver ID for Nicolas Kiesa.
-        /// </summary>
-        public const string NicolasKiesa = "kiesa";
-
-        /// <summary>
-        /// Driver ID for Olivier Panis.
-        /// </summary>
-        public const string OlivierPanis = "panis";
-
-        /// <summary>
-        /// Driver ID for Jos Verstappen.
-        /// </summary>
-        public const string JosVerstappen = "verstappen";
-
-        /// <summary>
-        /// Driver ID for Eddie Irvine.
-        /// </summary>
-        public const string EddieIrvine = "irvine";
-
-        /// <summary>
-        /// Driver ID for Mika Hakkinen.
-        /// </summary>
-        public const string MikaHakkinen = "hakkinen";
-
-        /// <summary>
-        /// Driver ID for Damon Hill.
-        /// </summary>
-        public const string DamonHill = "damon_hill";
-
-        /// <summary>
-        /// Driver ID for Graham Hill.
-        /// </summary>
-        public const string GrahamHill = "hill";
-
-        /// <summary>
-        /// Driver ID for Juan Fangio.
-        /// </summary>
-        public const string JuanFangio = "fangio";
-
-        /// <summary>
-        /// Driver ID for Alberto Ascari.
-        /// </summary>
-        public const string AlbertoAscari = "fangio";
-
-        /// <summary>
-        /// Driver ID for Jack Brabham.
-        /// </summary>
-        public const string JackBrabham = "jack_brabham";
-
-        /// <summary>
-        /// Driver ID for Jim Clark.
-        /// </summary>
-        public const string JimClark = "clark";
-
-        /// <summary>
-        /// Driver ID for John Surtees.
-        /// </summary>
-        public const string JohnSurtees = "surtees";
-
-        /// <summary>
-        /// Driver ID for Jackie Stewart.
-        /// </summary>
-        public const string JackieStewart = "stewart";
-
-        /// <summary>
-        /// Driver ID for Emerson Fittipaldi.
-        /// </summary>
-        public const string EmersonFittipaldi = "emerson_fittipaldi";
-
-        /// <summary>
-        /// Driver ID for Niki Lauda.
-        /// </summary>
-        public const string NikiLauda = "lauda";
-
-        /// <summary>
-        /// Driver ID for James Hunt.
-        /// </summary>
-        public const string JamesHunt = "hunt";
-
-        /// <summary>
-        /// Driver ID for Mario Andretti.
-        /// </summary>
-        public const string MarioAndretti = "mario_andretti";
-
-        /// <summary>
-        /// Driver ID for Nelson Piquet.
-        /// </summary>
-        public const string NelsonPiquet = "piquet";
-
-        /// <summary>
-        /// Driver ID for Keke Rosberg.
-        /// </summary>
-        public const string KekeRosberg = "keke_rosberg";
-
-        /// <summary>
-        /// Driver ID for Alain Prost.
-        /// </summary>
-        public const string AlainProst = "prost";
-
-        /// <summary>
-        /// Driver ID for Ayrton Senna.
-        /// </summary>
-        public const string AyrtonSenna = "senna";
-
-        /// <summary>
-        /// Driver ID for Nigel Mansell.
-        /// </summary>
-        public const string NigelMansell = "mansell";
+        public const string YukiTsunoda = "tsunoda";
     }
 }
