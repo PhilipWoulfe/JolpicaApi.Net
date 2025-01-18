c418a11bdd258594d9ae139d9c97574c71f38869
Philip Woulfe <philip.woulfe@gmail.com>
2025-01-18 21:40:26 +0000

    AB#15 fix some issues

--- src/ErgastApi/Abstractions/IHttpClient.cs
+++ src/ErgastApi/Abstractions/IHttpClient.cs
@@ -4,8 +4,16 @@ using System.Threading.Tasks;
 
 namespace ErgastApi.Abstractions
 {
+    /// <summary>
+    /// Represents an HTTP client for sending HTTP requests and receiving HTTP responses.
+    /// </summary>
     public interface IHttpClient : IDisposable
     {
+        /// <summary>
+        /// Sends a GET request to the specified URI as an asynchronous operation.
+        /// </summary>
+        /// <param name="requestUri">The URI to which the request is sent.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the HTTP response message.</returns>
         Task<HttpResponseMessage> GetAsync(string requestUri);
     }
 }
--- src/ErgastApi/Client/Attributes/UrlSegmentAttribute.cs
+++ src/ErgastApi/Client/Attributes/UrlSegmentAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace ErgastApi.Client.Attributes
 {
+    /// <summary>
+    /// Attribute to define URL segments for properties.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class UrlSegmentAttribute : Attribute
     {
@@ -10,18 +13,31 @@ namespace ErgastApi.Client.Attributes
         /// </summary>
         internal int? NullableOrder { get; set; }
 
+        /// <summary>
+        /// Gets or sets the order of the URL segment.
+        /// </summary>
         public int Order
         {
             get => NullableOrder ?? default(int);
             set => NullableOrder = value;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the URL segment.
+        /// </summary>
         public string SegmentName { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UrlSegmentAttribute"/> class.
+        /// </summary>
         public UrlSegmentAttribute()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UrlSegmentAttribute"/> class with a specified segment name.
+        /// </summary>
+        /// <param name="segmentName">The name of the URL segment.</param>
         public UrlSegmentAttribute(string segmentName)
         {
             SegmentName = segmentName;
