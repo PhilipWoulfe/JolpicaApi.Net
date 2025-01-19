b135950be1192b00fbec081f69558e5e5e4e346c
Philip Woulfe <philip.woulfe@gmail.com>
2025-01-19 15:40:39 +0000

    AB#15 update circuit and team ids

--- src/JolpicaApi/Ids/Constructors.cs
+++ src/JolpicaApi/Ids/Constructors.cs
@@ -5,44 +5,143 @@
     /// </summary>
     public static class Constructors
     {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        // Active teams
+        /// <summary>Alfa Romeo</summary>
+        /// <remarks>Started: 2019</remarks>
+        public const string AlfaRomeo = "alfa_romeo";
+        /// <summary>Alpine F1 Team</summary>
+        /// <remarks>Started: 2021</remarks>
+        public const string Alpine = "alpine";
+        /// <summary>Aston Martin</summary>
+        /// <remarks>Started: 2021</remarks>
+        public const string AstonMartin = "aston_martin";
+        /// <summary>Scuderia Ferrari</summary>
+        /// <remarks>Started: 1950</remarks>
         public const string Ferrari = "ferrari";
-        public const string ForceIndia = "force_india";
+        /// <summary>Haas F1 Team</summary>
+        /// <remarks>Started: 2016</remarks>
         public const string Haas = "haas";
-        public const string Mercedes = "mercedes";
-        public const string Manor = "manor";
-        public const string Marussia = "marussia";
+        /// <summary>McLaren</summary>
+        /// <remarks>Started: 1966</remarks>
         public const string McLaren = "mclaren";
+        /// <summary>Mercedes-Benz in Formula One</summary>
+        /// <remarks>Started: 2010</remarks>
+        public const string Mercedes = "mercedes";
+        /// <summary>RB F1 Team</summary>
+        /// <remarks>Started: 2024</remarks>
+        public const string Rb = "rb";
+        /// <summary>Red Bull Racing</summary>
+        /// <remarks>Started: 2005</remarks>
         public const string RedBull = "red_bull";
+        /// <summary>Renault</summary>
+        /// <remarks>Started: 1977</remarks>
         public const string Renault = "renault";
+        /// <summary>Sauber Motorsport</summary>
+        /// <remarks>Started: 1993</remarks>
         public const string Sauber = "sauber";
-        public const string ToroRosso = "toro_rosso";
+        /// <summary>Williams Grand Prix Engineering</summary>
+        /// <remarks>Started: 1977</remarks>
         public const string Williams = "williams";
-        /// <summary>2012-2015</summary>
-        public const string LotusF1 = "lotus_f1";
-        /// <summary>2011</summary>
-        public const string LotusRacing = "lotus_racing";
-        public const string Caterham = "caterham";
-        public const string HRT = "hrt";
-        public const string Virgin = "virgin";
-        public const string Brawn = "brawn";
-        public const string BmwSauber = "bmw_sauber";
-        public const string Toyota = "Toyota";
-        public const string SuperAguri = "super_aguri";
-        public const string Honda = "honda";
-        public const string Spyker = "spyker";
-        public const string SpykerMF1 = "spyker_mf1";
-        /// <summary>Midland F1 Racing</summary>
-        public const string MF1 = "mf1";
-        public const string BAR = "bar";
-        public const string Jordan = "jordan";
-        public const string Minardi = "minardi";
-        public const string Jaguar = "jaguar";
+
+        // Historic teams
+        /// <summary>AlphaTauri</summary>
+        /// <remarks>2020-2023</remarks>
+        public const string AlphaTauri = "alphatauri";
+        /// <summary>Arrows</summary>
+        /// <remarks>1978-2002</remarks>
         public const string Arrows = "arrows";
+        /// <summary>BAR</summary>
+        /// <remarks>1999-2005</remarks>
+        public const string BAR = "bar";
+        /// <summary>Benetton</summary>
+        /// <remarks>1986-2001</remarks>
         public const string Benetton = "benetton";
+        /// <summary>BMW Sauber</summary>
+        /// <remarks>2006-2009</remarks>
+        public const string BmwSauber = "bmw_sauber";
+        /// <summary>Brabham</summary>
+        /// <remarks>1962-1992</remarks>
+        public const string Brabham = "brabham";
+        /// <summary>Brawn GP</summary>
+        /// <remarks>2009</remarks>
+        public const string Brawn = "brawn";
+        /// <summary>Caterham</summary>
+        /// <remarks>2012-2014</remarks>
+        public const string Caterham = "caterham";
+        /// <summary>Force India</summary>
+        /// <remarks>2008-2018</remarks>
+        public const string ForceIndia = "force_india";
+        /// <summary>HRT</summary>
+        /// <remarks>2010-2012</remarks>
+        public const string HRT = "hrt";
+        /// <summary>Jaguar</summary>
+        /// <remarks>2000-2004</remarks>
+        public const string Jaguar = "jaguar";
+        /// <summary>Jordan</summary>
+        /// <remarks>1991-2005</remarks>
+        public const string Jordan = "jordan";
+        /// <summary>Ligier</summary>
+        /// <remarks>1976-1996</remarks>
+        public const string Ligier = "ligier";
+        /// <summary>Lotus</summary>
+        /// <remarks>1958-1994</remarks>
+        public const string Lotus = "lotus";
+        /// <summary>Lotus F1</summary>
+        /// <remarks>2012-2015</remarks>
+        public const string LotusF1 = "lotus_f1";
+        /// <summary>Lotus Racing</summary>
+        /// <remarks>2010-2011</remarks>
+        public const string LotusRacing = "lotus_racing";
+        /// <summary>Manor</summary>
+        /// <remarks>2010-2016</remarks>
+        public const string Manor = "manor";
+        /// <summary>Maserati</summary>
+        /// <remarks>1950-1957</remarks>
+        public const string Maserati = "maserati";
+        /// <summary>Matra</summary>
+        /// <remarks>1968-1972</remarks>
+        public const string Matra = "matra";
+        /// <summary>Midland F1 Racing</summary>
+        /// <remarks>2006</remarks>
+        public const string MF1 = "mf1";
+        /// <summary>Minardi</summary>
+        /// <remarks>1985-2005</remarks>
+        public const string Minardi = "minardi";
+        /// <summary>Osella</summary>
+        /// <remarks>1980-1990</remarks>
+        public const string Osella = "osella";
+        /// <summary>Prost</summary>
+        /// <remarks>1997-2001</remarks>
         public const string Prost = "prost";
+        /// <summary>Scuderia Toro Rosso</summary>
+        /// <remarks>2006-2019</remarks>
+        public const string ToroRosso = "toro_rosso";
+        /// <summary>Shadow</summary>
+        /// <remarks>1973-1980</remarks>
+        public const string Shadow = "shadow";
+        /// <summary>Spyker</summary>
+        /// <remarks>2007</remarks>
+        public const string Spyker = "spyker";
+        /// <summary>Spyker MF1</summary>
+        /// <remarks>2006</remarks>
+        public const string SpykerMF1 = "spyker_mf1";
+        /// <summary>Stewart</summary>
+        /// <remarks>1997-1999</remarks>
         public const string Stewart = "stewart";
+        /// <summary>Super Aguri</summary>
+        /// <remarks>2006-2008</remarks>
+        public const string SuperAguri = "super_aguri";
+        /// <summary>Toyota</summary>
+        /// <remarks>2002-2009</remarks>
+        public const string Toyota = "Toyota";
+        /// <summary>Tyrrell</summary>
+        /// <remarks>1970-1998</remarks>
         public const string Tyrrell = "Tyrrell";
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+        /// <summary>Vanwall</summary>
+        /// <remarks>1954-1960</remarks>
+        public const string Vanwall = "vanwall";
+        /// <summary>Virgin</summary>
+        /// <remarks>2010-2011</remarks>
+        public const string Virgin = "virgin";
     }
 }
