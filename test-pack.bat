170ed65a8a8c12f9f01af4a6253a6587a0c14849
PhilipWoulfe <philip.woulfe@gmail.com>
2025-01-19 15:53:05 +0000

    chore: AB#15 update version test script (#8)

--- test-pack.bat
+++ test-pack.bat
@@ -4,19 +4,7 @@
 dotnet tool install --global GitVersion.Tool
 
 :: Run GitVersion to get the version
-$VERSION=$(gitversion /showvariable SemVer)
+dotnet-gitversion /output json /showvariable SemVer
 
 :: Debug output
-echo "GitVersion SemVer: $VERSION"
-
-:: Restore dependencies
-dotnet restore
-
-:: Build the project
-dotnet build --no-restore --configuration Release
-
-:: Pack the NuGet package with the calculated version
-dotnet pack --no-build --configuration Release --output ./nupkg /p:PackageVersion=$VERSION
-
-:: Verify the package
-echo "NuGet package created in ./nupkg with version $VERSION"
+echo "$(dotnet-gitversion /showvariable SemVer)"
